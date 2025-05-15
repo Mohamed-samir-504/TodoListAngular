00721c23b8ba5741b4eab444cc288b521a969dcb
mtolba <mm.ahmedd504@gmail.com>
2025-05-15 20:36:54 +0300

    feat(login): Create login form

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  
+  private router = inject(Router);
 
+  loginForm = new FormGroup({
+    email: new FormControl('',{
+      validators:[Validators.email, Validators.required],
+    }),
+    password: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(6)],
+    }),
+  });
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      const { email, password } = this.loginForm.value;
+      console.log('Login submitted:', email, password);
+      // TODO: Call Firebase Auth service here
+    }
+  }
+
+  goToSignup(): void {
+    this.router.navigate(['/signup']);
+  }
 }
